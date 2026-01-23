72feeb0bb16800c8060bc133d2d3a071fb040dc3
bammytoye <bamigbalatoyese@gmail.com>
2026-01-23 12:05:14 +0100

    'NavBar'

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+//pages
+import {
+  Home,
+  About,
+  Contact,
+  Doctors,
+  Profile,
+  MyAppointment,
+  Appointment,
+  Login
+} from './pages';
+
+//components
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
-    <div className='text-green-500 bg-black text-center flex justify-center items-center h-screen'>
-      App
+    <div className='mx-4 sm:mx-[10%]'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={< About/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/doctors' element={<Doctors/>} />
+        <Route path='/doctors/:speciality' element={<Doctors/>} />
+        <Route path='/profile' element={<Profile/>} />
+        <Route path='/my-appointment' element={<MyAppointment/>} />
+        <Route path='/appointment/:docId' element={<Appointment/>} />
+        <Route path='/login' element={<Login/>} />
+      </Routes>
     </div>
   )
 }
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,8 @@
+export { default as Home } from './Home';
+export { default as About } from './About';
+export { default as Contact } from './Contact';
+export { default as Doctors } from './Doctors';
+export { default as Profile } from './Profile';
+export { default as MyAppointment } from './MyAppointment';
+export { default as Login } from './Login';
+export { default as Appointment } from './Appointment';
