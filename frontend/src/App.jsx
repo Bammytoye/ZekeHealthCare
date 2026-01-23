1dbfb7131f60bdc8e3d167fd935b574be47e0001
bammytoye <bamigbalatoyese@gmail.com>
2026-01-23 10:10:54 +0100

    'HomePage'

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-green-500 bg-black text-center flex justify-center items-center h-screen'>
+      App
+    </div>
+  )
+}
+
+export default App
