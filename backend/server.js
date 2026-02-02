966ecbec14897f8f3018a5e45cde34db3ea06d25
bammytoye <bamigbalatoyese@gmail.com>
2026-02-02 15:40:32 +0100

    'setingCloudinary''

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,25 @@
-import express from 'express' 
-import cors from 'cors'
-import 'dotenv/config.js'
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import connectDB from './config/MongoDB.js';
+import configureCloudinary from './config/couldinary.js';
 
 
-//app config
 const app = express();
 const port = process.env.PORT || 8000;
+connectDB();
+configureCloudinary();
 
-
-//middlewares
+// middlewares
 app.use(cors());
 app.use(express.json());
 
-//api endpoints
+// test route
 app.get('/', (req, res) => {
-    res.status(200).send('Booking Appointment Backend is running');
+    res.status(200).send('Booking Appointment Backend Running');
 });
 
-//listen 
+// listen
 app.listen(port, () => {
-    console.log('Booking Appointment Backend is running on port', port);
+    console.log(`Booking Appointment Backend running on port ${port}`);
 });
